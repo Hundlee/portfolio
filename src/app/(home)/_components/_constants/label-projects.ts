2c5f972b47582f16d3a75f2262bcc2f0618b72d3
Hundlee <mesquitag884@gmail.com>
2024-02-18 14:35:48 -0300

    feat: add converter project

--- src/app/(home)/_components/_constants/label-projects.ts
+++ src/app/(home)/_components/_constants/label-projects.ts
@@ -10,12 +10,12 @@ export interface ProjectProps {
 
 export const LABELSPROJECTS: ProjectProps[] = [
     {
-        name: "Portfolio",
+        name: "Portfólio",
         gitUrl: "https://github.com/Hundlee/portfolio",
         imageURL: "./portifolioProject.png",
         deploy: "https://github.com/Hundlee/portfolio",
-        alt: "Portfolio",
-        description: "Portfolio",
+        alt: "Portfólio",
+        description: "Portfólio",
         technologies: [
             "typescript-icon.svg",
             "react.svg",
@@ -29,12 +29,38 @@ export const LABELSPROJECTS: ProjectProps[] = [
         gitUrl: "https://github.com/Hundlee/chatReact",
         deploy: "https://chat-react-kohl.vercel.app/",
         alt: "ChatReact",
-        description: "Chat criando com ReactJs com o banco de dados Firebase",
+        description: "Chat criado com ReactJs e com Firebase",
         technologies: [
             "javascript-js.svg",
             "react.svg",
             "file-type-firebase.svg",
             "file-type-sass.svg",
+        ],
+    },
+    {
+        name: "Conversor de Moedas",
+        imageURL: "./converterApp.png",
+        gitUrl: "https://github.com/Hundlee/CorverterApp",
+        deploy: "https://corverter-app.vercel.app/",
+        alt: "ChatReact",
+        description: "Criando com JS",
+        technologies: [
+            "javascript-js.svg",
+            "file-type-html.svg",
+            "file-type-css.svg",
+        ],
+    },
+    {
+        name: "Gerador de Senha",
+        imageURL: "./passwordGenerator.png",
+        gitUrl: "https://github.com/Hundlee/CorverterApp",
+        deploy: "https://corverter-app.vercel.app/",
+        alt: "Gerador de Senha",
+        description: "Criando com JS",
+        technologies: [
+            "javascript-js.svg",
+            "file-type-html.svg",
+            "file-type-css.svg",
         ],
     },
 ];
