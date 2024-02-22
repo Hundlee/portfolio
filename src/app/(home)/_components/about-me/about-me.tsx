66e22476b8c0e957dbb3080902c386a062285f7b
Hundlee <mesquitag884@gmail.com>
2024-02-22 15:28:38 -0300

    style: add hyphens to text

--- src/app/(home)/_components/about-me/about-me.tsx
+++ src/app/(home)/_components/about-me/about-me.tsx
@@ -8,12 +8,12 @@ const AboutMe = () => {
                 Sobre mim
             </h1>
             <div>
-                <h2 className="max-w-[22rem] text-sm text-gray-200 text-justify sm:max-w-[30rem] md:max-w-[600px] md:text-xl lg:text-xl lg:max-w-[55rem] xl:max-w-[65rem] 2xl:max-w-[85rem] 2xl:text-2xl">
+                <h2 className="max-w-[22rem] hyphens-auto text-sm text-gray-200 text-justify sm:max-w-[30rem] md:max-w-[600px] md:text-xl lg:text-xl lg:max-w-[55rem] xl:max-w-[65rem] 2xl:max-w-[85rem] 2xl:text-2xl">
                     Olá! Meu nome é{" "}
                     <span className="text-white font-semibold">
                         Gabriel Mesquita
-                    </span>{" "}
-                    Tenho 20 anos e sou um desenvolvedor FullStack apaixonado
+                    </span>
+                    . Tenho 20 anos e sou um desenvolvedor FullStack apaixonado
                     por programação. Com mais de 2 anos de experiência dedicada
                     ao estudo e prática em desenvolvimento web. Estou
                     constantemente buscando desafios e oportunidades para
